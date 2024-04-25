7d6002b1fc8d689283db5f90f2dd898176bad7e5
ToanNguyen9oanNguyen9602 <toannguyen9602@gmail.com>
2024-04-25 17:46:35 +0700

    commit 25-04-P1

--- lession1/my-app-1/src/components/ExpenseItem.jsx
+++ lession1/my-app-1/src/components/ExpenseItem.jsx
@@ -1,18 +1,16 @@
+import Card from './Card';
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate2 = new Date(2024, 4, 14)
-    const expenseTitle2 = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
-            <div>{expenseDate2.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{expenseTitle2}</h2>
-                <div className='expense-item__price'>{expenseAmount}</div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>    
-    )
+        </Card>    
+    );
 }
 export default ExpenseItem
